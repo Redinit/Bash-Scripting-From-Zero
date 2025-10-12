9092365dfa40c5a957c8e1de93bdb933e8a55ecc
Redinit <redinit404@gmail.com>
2025-10-12 23:25:25 +0530

    Create echo_no_newline.sh

---
+++ Scripts/echo_printf_comments/Echo/echo_no_newline.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# Print Text Without NewLine
+echo -n "Hell0, "
+echo "World!" # Output Appear on The Same Line: Hello, World!
+
+# Combine Multiple echo -n Commands
+echo -n "Username: "
+echo "Redinit" # Output "Username: Redinit"
