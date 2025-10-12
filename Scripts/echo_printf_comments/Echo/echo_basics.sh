37298c222b50ca9d17c7c01c7660dc0be5003207
Redinit <redinit404@gmail.com>
2025-10-12 23:19:17 +0530

    Create echo_basics.sh

---
+++ Scripts/echo_printf_comments/Echo/echo_basics.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# Print a Single Message
+echo "Hello World!"
+
+# Print Mulitple Words
+echo "Welcome to Bash Scripting"
+
+# Print Text Stored in a Variable
+name="Redinit"
+echo "My Name is $name"
+
+# Print Multiple Lines Using Multiple Echo Commands
+echo "This is Line 1"
+echo "This is Line 2"
+
+# Combine Text and Variable in Same Line
+Language="Bash"
+echo "I am Learning $Language Scripting"
